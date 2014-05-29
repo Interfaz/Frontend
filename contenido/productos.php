975e5675d57a17898039d91628ce110c1f1778d1
Interfaz <danielg.garcia@yahoo.com>
2014-05-29 18:57:08 -0500

    Lista de productos

--- contenido/productos.php
+++ contenido/productos.php
@@ -6,5 +6,17 @@
 </head>
 
 <body>
+
+<h1>Productos</h1>
+
+<ul>
+  <li>Producto 1</li>
+  <li>Producto 2</li>
+  <li>Producto 3</li>
+  <li>Producto 4</li>
+  <li>Producto 5</li> 
+</ul>
+
+
 </body>
 </html>
